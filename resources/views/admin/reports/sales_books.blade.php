38dc51a0cba5529380a789e08b840f1017ce09b7
sirdax99 <sirdax99@gmail.com>
2022-03-15 13:59:35 -0400

    fecha store factura

--- resources/views/admin/reports/sales_books.blade.php
+++ resources/views/admin/reports/sales_books.blade.php
@@ -1,5 +1,3 @@
-
-  
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -22,6 +20,13 @@
     text-align: left;
   }
   </style>
+   
+  <style>
+  .page-break {
+      page-break-after: always;
+  }
+  </style>
+
 </head>
 
 <body>
@@ -145,10 +150,16 @@
     <th style="text-align: center; font-weight: normal; border-color: white;"></th>
   </tr> 
 </table>
-<br><br>
+<div class="page-break"></div>
+<table>
+  <tr>
+    <th style="text-align: left; font-weight: normal; width: 10%; border-color: white; font-weight: bold;"> <img src="{{ asset(Auth::user()->company->foto_company ?? 'img/northdelivery.jpg') }}" width="90" height="30" class="d-inline-block align-top" alt="">
+    </th>
+    <th style="text-align: left; font-weight: normal; width: 90%; border-color: white; font-weight: bold;"><h4>{{Auth::user()->company->razon_social ?? ''}}  <h5>{{Auth::user()->company->code_rif ?? ''}}</h5> </h4></th>
+  </tr> 
+</table> 
 
- 
-<table ALIGN="right" style="width: 50%;">
+<table ALIGN="right" style="width: 60%;">
   <tr>
     <th style="text-align: center; ">Resumen del periodo {{ $date_begin ?? '' }} hasta: {{ $date_end ?? '' }}</th>
     <th style="text-align: center; "></th>
@@ -157,88 +168,88 @@
     <th style="text-align: center; ">Retenciones</th>
   </tr> 
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total Ventas Internas No Gravadas</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Total Ventas Internas No Gravadas</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Notas de Credito  No Gravadas</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Notas de Credito  No Gravadas</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total Ventas Exportación</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Total Ventas Exportación</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total Ventas   Internas solo Alicuota General 16%</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal; font-style:bold;">Total Ventas   Internas solo Alicuota General 16%</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(16, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total Ventas  Internas solo Alicuota General mas Adicional </td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Total Ventas  Internas solo Alicuota General mas Adicional </td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total Ventas  Internas solo Alicuota Reducida</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Total Ventas  Internas solo Alicuota Reducida</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total de  Ventas y Debitos Fiscales</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal; font-style:bold;">Total de  Ventas y Debitos Fiscales</td>
+    <td style="text-align: right; font-weight: normal; font-style:bold;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal; font-style:bold;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal; font-style:bold;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal; font-style:bold;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Ajustes a los Débitos Fiscales de Períodos Anteriores:</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Ajustes a los Débitos Fiscales de Períodos Anteriores:</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(16, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Notas de Crédito Gravadas Alicuota General 16%</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Notas de Crédito Gravadas Alicuota General 16%</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(16, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total Notas de Crédito y Debito Gravadas </td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Total Notas de Crédito y Debito Gravadas </td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total Ajustes Débitos Fiscales de Períodos Anteriores:</td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Total Ajustes Débitos Fiscales de Períodos Anteriores:</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
   <tr>
-    <td style="text-align: right; font-weight: normal;">Total de Ventas y Débitos Fiscales Alicuota General </td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
-    <td style="text-align: right; font-weight: normal;"></td>
+    <td style="text-align: left; font-weight: normal;">Total de Ventas y Débitos Fiscales Alicuota General </td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
+    <td style="text-align: right; font-weight: normal;">{{number_format(0, 2, ',', '.')}}</td>
   </tr>
 </table>
 
