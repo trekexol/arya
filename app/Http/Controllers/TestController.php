7bf52437882c285fa6b70c345ae88dda58f85984
sirdax99 <sirdax99@gmail.com>
2022-03-17 16:01:06 -0400

    dependencia

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Test;
+
+use App\Http\Controllers\UserAccess\UserAccessController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TestController extends Controller
+{
+ 
+    public $userAccess;
+    public $modulo = 'Cotizacion';
+
+    public function __construct(){
+
+        $this->middleware('auth');
+        $this->userAccess = new UserAccessController();
+    }
+
+   public function index()
+   {
+        if($this->userAccess->validate_user_access($this->modulo)){
+            $user= auth()->user();
+
+            $msg = 'msg: ';
+            $urlToGet ='http://www.bcv.org.ve/bcv/contactos';
+            $pageDocument = @file_get_contents($urlToGet);
+            preg_match_all('|<div class="col-sm-6 col-xs-6 centrado"><strong> (.*?) </strong> </div>|s', $pageDocument, $cap);
+    
+            if ($cap[0] == array()){ // VALIDAR Concidencia
+                $titulo = '0,00';
+            } else {
+                $titulo = $cap[1][4];
+            }
+    
+            $bcv_con_formato = $titulo;
+            $bcv = str_replace(',', '.', str_replace('.', '',$bcv_con_formato));
+    
+    
+            /*-------------------------- */
+            $msg .= bcdiv($bcv, '1', 2);
+            
+            
+            return view('admin.test.index',compact('msg'));
+        }else{
+            return redirect('/home')->withDanger('No tiene Acceso al modulo de '.$this->modulo);
+        }
+   }
+
+   /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+  
+
+}
