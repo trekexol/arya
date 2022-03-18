0ebd19e88694255a2264e54023ce7bf5ca75d329
sirdax99 <sirdax99@gmail.com>
2022-03-18 16:25:27 -0400

    nueva consulta taza y factura fecha al cobrar

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -2203,27 +2203,6 @@ public function store(Request $request) // Guardar recibo solo
         
     }
 
-    public function search_bcv()
-    {
-        /*Buscar el indice bcv*/
-        $urlToGet ='http://www.bcv.org.ve/bcv/contactos';
-        $pageDocument = @file_get_contents($urlToGet);
-        preg_match_all('|<div class="col-sm-6 col-xs-6 centrado"><strong> (.*?) </strong> </div>|s', $pageDocument, $cap);
-
-        if ($cap[0] == array()){ // VALIDAR Concidencia
-            $titulo = '0,00';
-        }else {
-            $titulo = $cap[1][4];
-        }
-
-        $bcv_con_formato = $titulo;
-        $bcv = str_replace(',', '.', str_replace('.', '',$bcv_con_formato));
-
-
-        /*-------------------------- */
-       return bcdiv($bcv, '1', 2);
-
-    }
 
 
 }
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,34 +22,20 @@ class TestController extends Controller
         $this->userAccess = new UserAccessController();
     }
 
-   public function index(Clientg $clientg)
+   public function index()
    {
+
        if($this->userAccess->validate_user_access($this->modulo)){
             $user= auth()->user();
 
             $msg = 'msg: ';
-            /* $urlToGet ='http://www.bcv.org.ve/bcv/contactos';
-            $pageDocument = @file_get_contents($urlToGet);
-            preg_match_all('|<div class="col-sm-6 col-xs-6 centrado"><strong> (.*?) </strong> </div>|s', $pageDocument, $cap);
-    
-            if ($cap[0] == array()){ // VALIDAR Concidencia
-                $titulo = '0,00';
-            } else {
-                $titulo = $cap[1][4];
-            }
-    
-            $bcv_con_formato = $titulo;
-            $bcv = str_replace(',', '.', str_replace('.', '',$bcv_con_formato));
-    
-             $msg .= bcdiv($bcv, '1', 2);   
-            -------------------------- */
-           
-            $crawler = $clientg->request('GET', 'https://www.aryasoftware.net');
-           // $msg .= '';
-            dd($crawler);
             
-          
+            $global = new GlobalController; 
+            
+            $bcv = $global->search_bcv();
 
+
+            dd($bcv);
 
             return view('admin.test.index',compact('msg'));
         }else{
