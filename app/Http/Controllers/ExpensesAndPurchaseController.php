c54caea339bc004cd59427201f7c78fe255c2d81
trekexol <cefreitas.16@gmail.com>
2022-03-28 11:22:10 -0400

    df

--- app/Http/Controllers/ExpensesAndPurchaseController.php
+++ app/Http/Controllers/ExpensesAndPurchaseController.php
@@ -332,11 +332,7 @@ class ExpensesAndPurchaseController extends Controller
                     ->where('code_three', 3)
                     ->where('code_four',1)
                     ->where('code_five', '<>',0)
-<<<<<<< Updated upstream
-                    ->orderBy('description','ASC')
-=======
                     ->orderBy('description','asc')
->>>>>>> Stashed changes
                     ->get();
                 }else{
                     $accounts_inventory = Account::on(Auth::user()->database_name)->select('id','description')->where('code_one',5)
@@ -344,11 +340,7 @@ class ExpensesAndPurchaseController extends Controller
                     ->where('code_three', '<>',0)
                     ->where('code_four', '<>',0)
                     ->where('code_five', '<>',0)
-<<<<<<< Updated upstream
-                    ->orderBy('description','ASC')
-=======
                     ->orderBy('description','asc')
->>>>>>> Stashed changes
                     ->get();
                 }
                 
