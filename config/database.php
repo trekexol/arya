b5bd4da40e25f9b4f814c39124b5f0b4eb3a1e7a
trekexol <cefreitas.16@gmail.com>
2022-04-05 12:09:39 -0400

    tekno security

--- config/database.php
+++ config/database.php
@@ -395,6 +395,25 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],  
+        'teknosecurity' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('TEKNOSECURITY_HOST', '127.0.0.1'),
+            'port' => env('TEKNOSECURITY_PORT', '3306'),
+            'database' => env('TEKNOSECURITY_DATABASE', 'forge'),
+            'username' => env('TEKNOSECURITY_USERNAME', 'forge'),
+            'password' => env('TEKNOSECURITY_PASSWORD', ''),
+            'unix_socket' => env('TEKNOSECURITY_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],  
 
         'pgsql' => [
             'driver' => 'pgsql',
