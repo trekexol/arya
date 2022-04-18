123540cf5905fcb1816701ee5891d97f9b60934c
sirdax99 <sirdax99@gmail.com>
2022-04-18 17:47:44 -0400

    envio de recibo 2

--- app/Http/Controllers/Mail/ReceiptMailController.php
+++ app/Http/Controllers/Mail/ReceiptMailController.php
@@ -112,150 +112,9 @@ class ReceiptMailController extends Controller
     
     public function pdfQuotation($quotation,$coin)
     {  
-    
-        $pdf = App::make('dompdf.wrapper');
-
-        if(isset($quotation)){
-
-           $payment_quotations = ReceiptPayment::on(Auth::user()->database_name)
-                                       ->where('id_quotation',$quotation->id)
-                                       ->where('status',1)
-                                       ->get();
-
-           foreach($payment_quotations as $var){
-               $var->payment_type = $this->asignar_payment_type($var->payment_type);
-               if($coin == 'dolares'){
-                   $var->amount = $var->amount / $var->rate;
-               }
-           }
-
-
-            $inventories_quotations = DB::connection(Auth::user()->database_name)->table('products')
-               ->join('receipt_products', 'products.id', '=', 'receipt_products.id_inventory')
-               ->where('receipt_products.id_quotation',$quotation->id)
-               ->where('receipt_products.status','=','C')
-               ->orwhere('receipt_products.status','=','1')
-               ->select('products.*','receipt_products.price as price','receipt_products.rate as rate','receipt_products.discount as discount',
-               'receipt_products.amount as amount_quotation','receipt_products.retiene_iva as retiene_iva_quotation'
-               ,'receipt_products.retiene_islr as retiene_islr_quotation')
-               ->get(); 
-
-
-            $client = owners::on(Auth::user()->database_name) // buscar cliente
-           ->where('id','=',$quotation->id_client)
-           ->select('owners.*')
-           ->get()->first();
-
-           //Buscar Factura original
-           $quotationsorigin = Receipts::on(Auth::user()->database_name) // buscar facura original
-           ->orderBy('id' ,'asc') 
-           ->where('date_billing','<>',null)
-           ->where('type','=','F')
-           ->where('number_invoice','=',$quotation->number_invoice)
-           ->select('receipts.*')
-           ->get();
-
-           $inventories_quotationso = DB::connection(Auth::user()->database_name)->table('products')
-           ->join('receipt_products', 'products.id', '=', 'receipt_products.id_inventory')
-           ->where('receipt_products.id_quotation',$quotation->id)
-           ->where('receipt_products.status','=','C')
-           ->orwhere('receipt_products.status','=','1')
-           ->select('products.*','receipt_products.price as price','receipt_products.rate as rate','receipt_products.discount as discount',
-           'receipt_products.amount as amount_quotation','receipt_products.retiene_iva as retiene_iva_quotation'
-           ,'receipt_products.retiene_islr as retiene_islr_quotation')
-           ->get();
-           
-           
-           //Buscar recibos que debe
-            $quotationp = Receipts::on(Auth::user()->database_name) // buscar facura original
-            ->orderBy('id' ,'asc') 
-            ->where('date_billing','<>',null)
-            ->where('type','=','R')
-            ->where('status','=','P')
-            ->where('id','!=',$quotation->id)
-            ->where('receipts.id_client','=',$client->id)
-            ->select('receipts.*')
-            ->get();
-
-
-           if (isset($quotationp)) {
- 
-                foreach ($quotationp as $quotationtpp) {
-
-                    $inventories_quotationsp = DB::connection(Auth::user()->database_name)->table('products') // productos recibo pendiente
-                    ->join('receipt_products', 'products.id', '=', 'receipt_products.id_inventory')
-                    ->where('receipt_products.id_quotation',$quotationtpp->id)
-                    ->where('receipt_products.status','=','C')
-                    ->orwhere('receipt_products.status','=','1')
-                    ->select('products.*','receipt_products.id_quotation as id_quotation','receipt_products.price as price','receipt_products.rate as rate','receipt_products.discount as discount',
-                    'receipt_products.amount as amount_quotation','receipt_products.retiene_iva as retiene_iva_quotation'
-                    ,'receipt_products.retiene_islr as retiene_islr_quotation', )
-                    ->get();
-
-                }
-
-
-                if(isset($inventories_quotationsp)){
-                    foreach ($inventories_quotationsp as $varp) {
-                        $quotationpn = Receipts::on(Auth::user()->database_name) // buscar facura original
-                        ->orderBy('id' ,'asc') 
-                        ->where('date_billing','<>',null)
-                        ->where('type','=','R')
-                        ->where('status','=','P')
-                        ->where('receipts.id_client','=',$client)
-                        ->select('number_delivery_note','date_billing')
-                        ->get()->first();
-                        $varp->number_delivery_note = $quotationpn->number_delivery_note;
-                        $varp->date_billing = $quotationpn->date_billing;
-                    }
-                } else {
-                    $inventories_quotationsp = null;
-                }
-
-
-            } else {
-                
-                $inventories_quotationsp = null;
-
-
-            }
-
-
-           if($coin == 'bolivares'){
-               $bcv = null;
-               
-           }else{
-               $bcv = $quotation->bcv;
-           }
-
-           $company = Company::on(Auth::user()->database_name)->find(1);
-           
-          // $lineas_cabecera = $company->format_header_line;
-
-           $pdf = $pdf->loadView('pdf.receipt',compact('company','quotation','inventories_quotations','payment_quotations','bcv','coin','quotationsorigin','inventories_quotationso','client','quotationp','inventories_quotationsp'));
-
-           return $pdf->output();
-        }
- 
-    }
-
-
-    function imprimirecibo($id_quotation,$coin = null) /// recibo de condominio
-    {
-      
 
         $pdf = App::make('dompdf.wrapper');
 
-        
-             $quotation = null;
-                 
-             if(isset($id_quotation)){
-                 $quotation = Receipts::on(Auth::user()->database_name)->where('date_billing', '<>', null)->find($id_quotation);
-              
-                                     
-             }else{
-                return redirect('/receipt/receipt')->withDanger('No llega el numero del recibo de condominio');
-                } 
      
              if(isset($quotation)){
 
@@ -351,13 +210,13 @@ class ReceiptMailController extends Controller
                             $varp->date_billing = $quotationpn->date_billing;
                         }
                     } else {
-                        $inventories_quotationsp = 0;
+                        $inventories_quotationsp = null;
                     }
 
 
                 } else {
                       
-                    $inventories_quotationsp = 0;
+                    $inventories_quotationsp = null;
 
 
                 }
