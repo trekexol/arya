4708bcf564f3cd0319dea73f62b464f72ab05f82
sirdax99 <sirdax99@gmail.com>
2022-04-22 14:53:39 -0400

    ajsute notas de entrega excel

--- resources/views/export_excel/accounts_receivable_note.blade.php
+++ resources/views/export_excel/accounts_receivable_note.blade.php
@@ -107,14 +107,14 @@
       <th style="text-align: center; font-weight: normal;">{{ $quotation->number_pedido ?? ''}}</th>
 
       @if(isset($coin) && $coin == 'bolivares')
-        <th style="text-align: right; font-weight: normal;">{{ number_format(($quotation->amount_with_iva ?? 0), 2, ',', '.') }}</th>
-        <th style="text-align: right; font-weight: normal;">{{ number_format(($quotation->amount_anticipo ?? 0), 2, ',', '.') }}</th>
+        <th style="text-align: right; font-weight: normal;">{{ number_format(($quotation->amount_with_iva ?? 0), 2) }}</th>
+        <th style="text-align: right; font-weight: normal;">{{ number_format(($quotation->amount_anticipo ?? 0), 2) }}</th>
         <th style="text-align: right; font-weight: normal;">{{ number_format($por_cobrar, 2, ',', '.') }}</th>
         @endif
         @if(isset($coin) && $coin == 'dolares')
-          <th style="text-align: right; font-weight: normal;">${{ number_format(($quotation->amount_with_iva ?? 0), 2, ',', '.') }}</th>
-          <th style="text-align: right; font-weight: normal;">${{ number_format(($quotation->amount_anticipo ?? 0), 2, ',', '.') }}</th>
-          <th style="text-align: right; font-weight: normal;">${{ number_format($por_cobrar, 2, ',', '.') }}</th>
+          <th style="text-align: right; font-weight: normal;">{{ number_format(($quotation->amount_with_iva ?? 0), 2) }}</th>
+          <th style="text-align: right; font-weight: normal;">{{ number_format(($quotation->amount_anticipo ?? 0), 2) }}</th>
+          <th style="text-align: right; font-weight: normal;">{{ number_format($por_cobrar, 2) }}</th>
         @endif 
 
    
@@ -138,14 +138,14 @@
 
      
     @if(isset($coin) && $coin == 'bolivares')
-      <th style="text-align: right; font-weight: normal;">{{ number_format(($total_por_facturar ?? 0), 2, ',', '.') }}</th>
+      <th style="text-align: right; font-weight: normal;">{{ number_format(($total_por_facturar ?? 0), 2) }}</th>
       <th style="text-align: right; font-weight: normal; border-color: white; border-right-color: black;"> Bs.</th>
       <th style="text-align: right; font-weight: normal;">{{ number_format($total_por_cobrar, 2, ',', '.') }}</th>
       @endif
       @if(isset($coin) && $coin == 'dolares')
-        <th style="text-align: right; font-weight: normal;">${{ number_format(($total_por_facturar ?? 0), 2, ',', '.') }}</th>
+        <th style="text-align: right; font-weight: normal;">{{ number_format(($total_por_facturar ?? 0), 2, ',','') }}</th>
         <th style="text-align: right; font-weight: normal; border-color: white; border-right-color: black;">USD</th>
-        <th style="text-align: right; font-weight: normal;">${{ number_format($total_por_cobrar, 2, ',', '.') }}</th>
+        <th style="text-align: right; font-weight: normal;">{{ number_format($total_por_cobrar, 2, ',','') }}</th>
         @endif 
   
   </tr> 
