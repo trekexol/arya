7c42b8ea93cc7a530d02a4dfecb6f8755b8a951e
sirdax99 <sirdax99@gmail.com>
2022-04-25 15:58:41 -0400

    ajuste reporte excel NE

--- resources/views/admin/professions/create.blade.php
+++ resources/views/admin/professions/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Registro de Profesión</div>
+                <div class="card-header">Registro de Tipo de Empleado</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('professions.store') }}">
