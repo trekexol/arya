e0b0dee6718bfd66069d1001398408b1c5589663
sirdax99 <sirdax99@gmail.com>
2022-07-03 00:26:18 -0400

    fotos productos VIII

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -452,8 +452,9 @@
 <link href="{{ ''/*asset("vendor/bootstrap-fileinput/css/fileinput.min.css")*/}}" rel="stylesheet" type="text/css"/>-->
 
 <script src="{{''/*asset('assets/pages/script/imagen/foto.js')*/}}"></script>
-<script src="{{asset('public/assets/pages/script/imagen/foto.js')}}"></script>
-<script src="{{asset('../assets/pages/script/imagen/foto.js')}}"></script>       
+
+<script src="{{asset('./assets/pages/script/imagen/foto.js')}}"></script> 
+      
 
 <script>    
         $("#type").on('change',function(){
