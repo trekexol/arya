d26d020694e9353a7626ab2422329871b7bcd005
sirdax99 <sirdax99@gmail.com>
2022-07-11 16:00:54 -0400

    creacion empresa NDCORP

--- config/database.php
+++ config/database.php
@@ -570,7 +570,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-        'medical_group' => [
+        'ndcorp' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('NDCORP_HOST', '127.0.0.1'),
