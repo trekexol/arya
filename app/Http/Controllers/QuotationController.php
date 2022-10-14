5435a1f01f984a2125a8941b126254c5b732a081
sirdax99 <sirdax99@gmail.com>
2022-10-14 06:55:13 -0400

    tasa bcv consulta errada

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -1124,8 +1124,8 @@ class QuotationController extends Controller
  
          if(isset($quotation)){
            
-            $inventories_quotations = DB::connection(Auth::user()->database_name)->table('products')->join('inventories', 'products.id', '=', 'inventories.product_id')
-                                                            ->join('quotation_products', 'inventories.id', '=', 'quotation_products.id_inventory')
+            $inventories_quotations = DB::connection(Auth::user()->database_name)->table('products')
+                                                            ->join('quotation_products', 'products.id', '=', 'quotation_products.id_inventory')
                                                             ->where('quotation_products.id_quotation',$quotation->id)
                                                             ->whereIn('quotation_products.status',['1','C'])
                                                             ->select('products.*','quotation_products.price as price','quotation_products.rate as rate','quotation_products.discount as discount',
