ed8de8ee603606991478284715a32b0e53fc2423
trekexol <cefreitas.16@gmail.com>
2022-10-25 08:37:12 -0400

    fusionando II

--- config/database.php
+++ config/database.php
@@ -106,6 +106,26 @@ return [
             ]) : [],
         ],
 
+        'aryanew' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('ARYANEW_HOST', '127.0.0.1'),
+            'port' => env('ARYANEW_PORT', '3306'),
+            'database' => env('ARYANEW_DATABASE', 'forge'),
+            'username' => env('ARYANEW_USERNAME', 'forge'),
+            'password' => env('ARYANEW_PASSWORD', ''),
+            'unix_socket' => env('ARYANEW_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'alimentos_jvg' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
