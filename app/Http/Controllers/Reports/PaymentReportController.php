742c7647185158eb75365caa6cf0ececa227dbc5
trekexol <cefreitas.16@gmail.com>
2023-03-07 18:31:24 -0400

    boton de moneda para reportes de pago de compras

--- app/Http/Controllers/Reports/PaymentReportController.php
+++ app/Http/Controllers/Reports/PaymentReportController.php
@@ -18,24 +18,24 @@ class PaymentLicReportController extends Controller
     public $userAccess;
     public $modulo = 'Reportes';
 
- 
+
     public function __construct(){
 
        $this->middleware('auth');
        $this->userAccess = new UserAccessController();
    }
 
-   
+
     public function index($typeperson,$id_client_or_vendor = null)
-    {        
-      
+    {
+
         $userAccess = new UserAccessController();
 
         if($userAccess->validate_user_access($this->modulo)){
             $date = Carbon::now();
-            $datenow = $date->format('Y-m-d');   
-            $client = null; 
-            $vendor = null; 
+            $datenow = $date->format('Y-m-d');
+            $client = null;
+            $vendor = null;
 
 
             if(isset($typeperson) && $typeperson == 'Cliente'){
@@ -47,7 +47,7 @@ class PaymentLicReportController extends Controller
                     $vendor    = Vendor::on(Auth::user()->database_name)->find($id_client_or_vendor);
                 }
             }
-            
+
             return view('admin.reports.paymentslic.index_payments',compact('client','datenow','typeperson','vendor'));
         }else{
             return redirect('/home')->withDanger('No tiene Acceso al modulo de '.$this->modulo);
@@ -56,7 +56,7 @@ class PaymentLicReportController extends Controller
 
     public function store(Request $request)
     {
-        
+
         $date_end = request('date_end');
         $type = request('type');
         $id_client = request('id_client');
@@ -66,7 +66,7 @@ class PaymentLicReportController extends Controller
         $vendor = null;
         $typeperson = 'ninguno';
 
-        
+
 
         if($type != 'todo'){
             if(isset($id_client)){
@@ -80,34 +80,34 @@ class PaymentLicReportController extends Controller
                 $id_client_or_vendor = $vendor;
             }
         }
-        
-        
+
+
         return view('admin.reports.paymentslic.index_payments',compact('coin','date_end','client','vendor','typeperson'));
     }
 
     function pdf($coin,$date_end,$typeperson,$id_client_or_vendor = null)
     {
-       
+
         $pdf = App::make('dompdf.wrapper');
         $quotation_payments = null;
-        
+
         $date = Carbon::now();
-        $datenow = $date->format('d-m-Y'); 
+        $datenow = $date->format('d-m-Y');
         if(empty($date_end)){
             $date_end = $datenow;
 
-            $date_consult = $date->format('Y-m-d'); 
+            $date_consult = $date->format('Y-m-d');
         }else{
             $date_end = Carbon::parse($date_end)->format('d-m-Y');
 
             $date_consult = Carbon::parse($date_end)->format('Y-m-d');
         }
 
-       
-        $period = $date->format('Y'); 
-        
+
+        $period = $date->format('Y');
+
         if(isset($typeperson) && ($typeperson == 'Cliente')){
-           
+
             $quotation_payments = DB::connection(Auth::user()->database_name)->table('quotations')
                                 ->leftjoin('clients', 'clients.id','=','quotations.id_client')
                                 ->leftjoin('vendors', 'vendors.id','=','quotations.id_vendor')
@@ -116,12 +116,12 @@ class PaymentLicReportController extends Controller
                                 ->where('quotations.amount','<>',null)
                                 ->where('quotations.date_quotation','<=',$date_consult)
                                 ->where('quotations.id_client',$id_client_or_vendor)
-                                
+
                                 ->select('quotation_payments.*','quotations.number_invoice','clients.name as name_client','accounts.description as description_account')
                                 ->orderBy('quotation_payments.id','desc')
                                 ->get();
-          
-              
+
+
         }else if(isset($typeperson) && $typeperson == 'Vendedor'){
 
             $quotation_payments = DB::connection(Auth::user()->database_name)->table('quotations')
@@ -132,11 +132,11 @@ class PaymentLicReportController extends Controller
                     ->where('quotations.amount','<>',null)
                     ->where('quotations.date_quotation','<=',$date_consult)
                     ->where('quotations.id_vendor',$id_client_or_vendor)
-                    
+
                     ->select('quotation_payments.*','quotations.number_invoice','clients.name as name_client','accounts.description as description_account')
                     ->orderBy('quotation_payments.id','desc')
                     ->get();
-                
+
         }else{
             $quotation_payments = DB::connection(Auth::user()->database_name)->table('quotations')
                     ->leftjoin('clients', 'clients.id','=','quotations.id_client')
@@ -149,33 +149,33 @@ class PaymentLicReportController extends Controller
                     ->get();
         }
 
-       
+
         foreach($quotation_payments as $var){
             $var->payment_type = $this->asignar_payment_type($var->payment_type);
-           
+
         }
-      
+
         $pdf = $pdf->loadView('admin.reports.paymentslic.payments',compact('coin','quotation_payments','datenow','date_end'));
         return $pdf->stream();
-                 
+
     }
 
     public function selectClient()
-    { 
+    {
         $clients    = Client::on(Auth::user()->database_name)->get();
-    
+
         return view('admin.reports.paymentslic.selectclient',compact('clients'));
     }
 
     public function selectVendor()
     {
         $vendors    = Vendor::on(Auth::user()->database_name)->get();
-    
+
         return view('admin.reports.paymentslic.selectvendor',compact('vendors'));
     }
 
     function asignar_payment_type($type){
-      
+
         if($type == 1){
             return "Cheque";
         }
