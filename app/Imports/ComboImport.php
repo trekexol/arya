8a13963bea43ae5bc5634ce0aa40fbc5064d27c1
sirdax99 <sirdax99@gmail.com>
2023-04-17 15:29:39 -0400

    facturacion couriertool 7

--- app/Imports/ComboImport.php
+++ app/Imports/ComboImport.php
@@ -5,7 +5,6 @@ namespace App\Imports;
 use App\ExpensesDetail;
 use App\Product;
 use App\Account;
-use App\ComboProduct;
 use App\Http\Controllers\GlobalController;
 use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\ToModel;
@@ -30,25 +29,15 @@ class ComboImport implements ToModel,WithHeadingRow, SkipsOnError
         $user       =   auth()->user();
         $date = Carbon::now();
 
-
+        
         $buscar_product = Product::on(Auth::user()->database_name)
         ->where('type','COMBO')
-        ->where('id',$row['id_combo'])
-        ->get();
+        ->find($row['id_combo']);
 
 
 
-        if($row['cantidad_producto'] == 0 OR $row['cantidad_producto'] == null){
-            $row['cantidad_producto'] = 1;
-        }
-
-        if($row['precio_venta_combo'] == 0 OR $row['precio_venta_combo'] == null){
-            $row['precio_venta_combo'] = 1;
-        }
-
-
-        if ($buscar_product->count() == 0) { // si no hay producto TIPO COMBO lo crea.
-
+        if (empty($buscar_product)) {
+            
             $costo_calculado = 0;
 
             $account = Account::on(Auth::user()->database_name)
@@ -56,78 +45,56 @@ class ComboImport implements ToModel,WithHeadingRow, SkipsOnError
             ->first();
 
             if (isset($account)) {
-                $id_account = $account->id;
+                $id_account = $account->id;    
             } else {
                 $id_account = 25;
             }
 
              $product = DB::connection(Auth::user()->database_name)->table('products')->insert([
                  'id'                    => $row['id_combo'],
-                 'segment_id'            => 1,
-                 'subsegment_id'         => null,
-                 'twosubsegment_id'      => null,
+                 'segment_id'            => 1, 
+                 'subsegment_id'         => null, 
+                 'twosubsegment_id'      => null, 
                  'threesubsegment_id'    => null,
                  'id_account'            => $id_account,
-                 'unit_of_measure_id'    => 1,
-                 'code_comercial'        => $row['codigo_comercial_combo'],
-                 'type'                  => 'COMBO',
-                 'description'           => $row['nombre_combo'],
-                 'price'                 => $row['precio_venta_combo'],
-                 'price_buy'             => $costo_calculado,
-                 'cost_average'          => 0,
-                 'photo_product'         => null,
-                 'money'                 => 'D',
-                 'exento'                => 0,
-                 'islr'                  => 0,
+                 'unit_of_measure_id'    => 1, 
+                 'code_comercial'        => $row['codigo_comercial_combo'], 
+                 'type'                  => 'COMBO', 
+                 'description'           => $row['nombre_combo'], 
+                 'price'                 => $row['precio_venta_combo'], 
+                 'price_buy'             => $costo_calculado, 
+                 'cost_average'          => 0, 
+                 'photo_product'         => null, 
+                 'money'                 => 'D', 
+                 'exento'                => 0, 
+                 'islr'                  => 0, 
                  'id_user'               => $user->id,
                  'special_impuesto'      => 0,
                  'status'                => 1,
                  'created_at'            => $date,
                  'updated_at'            => $date
-             ]);
+             ]);  
 
-            /* $global = new GlobalController;
-             $global->transaction_inv('creado',$row['id_combo'],'Importacion Masiva de Combos',0,$row['precio_venta_combo'],$date,1,1,0,0,0,0,0);*/
+             $global = new GlobalController; 
+             $global->transaction_inv('creado',$row['id_combo'],'Importacion Masiva de Combos',0,$row['precio_venta_combo'],$date,1,1,0,0,0,0,0);
 
         }
 
-        /***SE BUSCA SI EL COMBO EXISTE CON EL PRODUCTO */
-        $buscarcombo = DB::connection(Auth::user()->database_name)
-                        ->table('combo_products')
-                        ->where('id_combo',$row['id_combo'])
-                        ->where('id_product',$row['id_producto'])
-                        ->get();
+        if ($row['id_combo'] && $row['id_producto'] != '') {
 
-        /*****SI EXISTE SE ACTUALIZA */
-        if($buscarcombo->count() > 0){
-
-
-            ComboProduct::on(Auth::user()->database_name)
-            ->where('id_combo',$row['id_combo'])
-            ->where('id_product',$row['id_producto'])
-            ->update(['amount_per_product' => $row['cantidad_producto']]);
-
-            Product::on(Auth::user()->database_name)
-            ->where('id',$row['id_combo'])
-            ->update(['price' => $row['precio_venta_combo']]);
-
-        }else{
-               /*****SI NO SE CREA */
             $combo_product = DB::connection(Auth::user()->database_name)->table('combo_products')->insert([
-                'id_combo'              => $row['id_combo'],
-                'id_product'            => $row['id_producto'],
-                'amount_per_product'    => $row['cantidad_producto']
-
-            ]);
-
+                'id_combo'              => $row['id_combo'],  
+                'id_product'            => $row['id_producto'], 
+                'amount_per_product'    => $row['cantidad_producto']   
+            
+            ]); 
         }
-
 
 
          return;
     }
 
-
+    
 
 
 
