77db28280c713d6ab78ca6a548100a819cfbbe38
sirdax99 <sirdax99@gmail.com>
2023-05-31 09:28:55 -0400

    Emprasa frigosur bd

--- resources/views/admin/companies/edit.blade.php
+++ resources/views/admin/companies/edit.blade.php
@@ -40,13 +40,7 @@
                         <label for="login" class="col-sm-2 col-form-label">Login(*)</label>
 
                         <div class="col-sm-4">
-                            <?php
-                            if ($company->login == 'nestornarvaez'){
-                                
-                                $company->login = 'frigosur';
-                            
-                            }
-                            ?>
+
                             
                             <input id="login" type="text" class="form-control @error('login') is-invalid @enderror" name="Login" value="{{ $company->login }}" required autocomplete="login" autofocus>
 
