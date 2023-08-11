91884103c50211bca18ad373ae9903c0c634e8a8
sirdax99 <sirdax99@gmail.com>
2023-08-11 10:01:38 -0400

    nueva empresa visionmv 22

--- config/database.php
+++ config/database.php
@@ -943,6 +943,26 @@ return [
                     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 ]) : [],
             ],
+            
+            'visionvm' => [
+                'driver' => 'mysql',
+                'url' => env('DATABASE_URL'),
+                'host' => env('VISIONVM_HOST', '127.0.0.1'),
+                'port' => env('VISIONVM_PORT', '3306'),
+                'database' => env('VISIONVM_DATABASE', 'forge'),
+                'username' => env('VISIONVM_USERNAME', 'forge'),
+                'password' => env('VISIONVM_PASSWORD', ''),
+                'unix_socket' => env('VISIONVM_SOCKET', ''),
+                'charset' => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix' => '',
+                'prefix_indexes' => true,
+                'strict' => true,
+                'engine' => null,
+                'options' => extension_loaded('pdo_mysql') ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ]) : [],
+            ],
 
         'pgsql' => [
             'driver' => 'pgsql',
