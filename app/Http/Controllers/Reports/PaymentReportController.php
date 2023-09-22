09dd4fb1c0829709625ca6fa3ecaab2485a41959
sirdax99 <sirdax99@gmail.com>
2023-09-22 13:58:32 -0400

    PaymentReportController

--- app/Http/Controllers/Reports/PaymentReportController.php
+++ app/Http/Controllers/Reports/PaymentReportController.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
-class PaymentLicReportController extends Controller
+class PaymentReportController extends Controller
 {
     public $userAccess;
     public $modulo = 'Reportes';
