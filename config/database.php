7f491f4b421b10d2195d42694b7bb57a5d1508e6
sirdax99 <sirdax99@gmail.com>
2023-10-18 11:53:15 -0400

    empresa famivar

--- config/database.php
+++ config/database.php
@@ -1004,6 +1004,27 @@ return [
                 ]) : [],
             ],
 
+            'famivar' => [
+                'driver' => 'mysql',
+                'url' => env('DATABASE_URL'),
+                'host' => env('FAMIVAR_HOST', '127.0.0.1'),
+                'port' => env('FAMIVAR_PORT', '3306'),
+                'database' => env('FAMIVAR_DATABASE', 'forge'),
+                'username' => env('FAMIVAR_USERNAME', 'forge'),
+                'password' => env('FAMIVAR_PASSWORD', ''),
+                'unix_socket' => env('FAMIVAR_SOCKET', ''),
+                'charset' => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix' => '',
+                'prefix_indexes' => true,
+                'strict' => true,
+                'engine' => null,
+                'options' => extension_loaded('pdo_mysql') ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                ]) : [],
+            ],
+
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
