0018d6a9aa4c57e77f65f69b66efd816a333b67b
trekexol <cefreitas.16@gmail.com>
2023-11-28 13:51:39 -0400

    cambios restaurante

--- resources/views/admin/restaurante/pedidomesa.blade.php
+++ resources/views/admin/restaurante/pedidomesa.blade.php
@@ -11,17 +11,25 @@ if($tipo == 'agregar'){
 
         <div class="table-responsive-lg">
 <table class="table table-light2 table-bordered dataTablematch" id="dataTablematch" >
-    <small><input type="text"  class="form-control form-control-sm filter-input" placeholder="Buscar Producto" /></small>
+
+        <small><select class="form-control form-control-sm filter-input">
+            <option value="todo">Todo.</option>
+        @foreach ($segmentos as $segmentos)
+        @if($segmentos->amount > 0)
+            <option value="{{$segmentos->segments['description']}}">{{$segmentos->segments['description']}}</option>
+        @endif
+        @endforeach
+        </select>
+        </small>
 
     <thead>
     <tr>
         <th>Producto</th>
+        <th>Tipo</th>
         <th>Cant. Inventario</th>
         <th>Monto</th>
         <th>---</th>
         <th>Cantidad</th>
-
-
     </tr>
     </thead>
     <tbody>
@@ -34,6 +42,7 @@ if($tipo == 'agregar'){
 
 
             <td><small>{{ $var->description}}</small></td>
+            <td><small>{{$var->segments['description']}}</small></td>
             <td><small>{{ $var->amount ?? 0}}</small></td>
             <td><small><input  class="form-control form-control-sm precio" name="precio[]" id="precio" type="number" value="{{ $var->price ?? 0}}" /></small></td>
             <td>
@@ -82,13 +91,21 @@ $(document).ready(function(){
         "bAutoWidth": false
         })
 
-$('.filter-input').keyup(function(){
+$('.filter-input').change(function(){
 
-tabladata.column(0)
-.search($(this).val())
-.draw();
+    var dato = $(this).val();
 
+    if(dato == 'todo'){
+        tabladata.column(1).search("").draw();
+    }else{
+        tabladata.column(1)
+        .search($(this).val())
+        .draw();
+    }
 });
+
+
+
 
     $("#carrito").validate({
       submitHandler: function (form) {
@@ -153,7 +170,7 @@ elseif($tipo == 'editar'){
           <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Actualizar Pedido</a>
         </li>
 </div>
-<div class="modal-body" >
+<div class="modal-body" align="center">
 
 
 
@@ -167,7 +184,7 @@ elseif($tipo == 'editar'){
                     <thead>
                         <tr>
                           <th scope="col">Producto</th>
-                          <th scope="col">Cantida</th>
+                          <th scope="col">Cantidad</th>
                           <th scope="col">Precio</th>
                           <th scope="col">Total</th>
                         </tr>
@@ -206,11 +223,20 @@ elseif($tipo == 'editar'){
 
                 <div class="table-responsive-lg">
         <table class="table table-light2 table-bordered dataTablematch" id="dataTablematch" >
-            <small><input type="text"  class="form-control form-control-sm filter-input" placeholder="Buscar Producto" /></small>
+                <small><select class="form-control form-control-sm filter-input">
+        <option value="todo">Todo.</option>
+    @foreach ($segmentos2 as $segmentos)
+    @if($segmentos->amount > 0)
+        <option value="{{$segmentos->segments['description']}}">{{$segmentos->segments['description']}}</option>
+    @endif
+    @endforeach
+    </select>
+    </small>
 
             <thead>
             <tr>
                 <th>Producto</th>
+                <th>Tipo</th>
                 <th>Cant. Inventario</th>
                 <th>Monto</th>
                 <th>---</th>
@@ -229,6 +255,7 @@ elseif($tipo == 'editar'){
 
 
                     <td><small>{{ $var->description}}</small></td>
+                    <td><small>{{$var->segments['description']}}</small></td>
                     <td><small>{{ $var->amount ?? 0}}</small></td>
                     <td><small><input  class="form-control form-control-sm precio" name="precio[]" id="precio" type="number" value="{{ $var->price ?? 0}}" /></small></td>
                     <td>
@@ -286,12 +313,17 @@ elseif($tipo == 'editar'){
             "bAutoWidth": false
             })
 
-    $('.filter-input').keyup(function(){
+    $('.filter-input').change(function(){
 
-    tabladata.column(0)
-    .search($(this).val())
-    .draw();
+    var dato = $(this).val();
 
+    if(dato == 'todo'){
+        tabladata.column(1).search("").draw();
+    }else{
+        tabladata.column(1)
+        .search($(this).val())
+        .draw();
+    }
     });
 
         $("#upcarrito").validate({
