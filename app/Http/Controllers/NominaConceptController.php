5724c94fc1a219483fdc67a593e4f5a7dbaea5d3
trekexol <cefreitas.16@gmail.com>
2023-11-29 16:18:48 -0400

    cambios nomina 4

--- app/Http/Controllers/NominaConceptController.php
+++ app/Http/Controllers/NominaConceptController.php
@@ -25,7 +25,7 @@ class NominaConceptController extends Controller
      */
     public function index()
     {
-        
+
         $user       =   auth()->user();
         $users_role =   $user->role_id;
 
@@ -35,10 +35,10 @@ class NominaConceptController extends Controller
        ->orderBy('type', 'asc')
         ->orderBy('calculate', 'desc')*/
         ->get();
-        
+
         if (isset($nominaconcepts)){
             foreach ($nominaconcepts as $nominaconcept) {
-            
+
                 $accounts = Account::on(Auth::user()->database_name)->orderBy('code_one', 'asc')
                 ->where('description','LIKE','%'.$nominaconcept->account_name.'%')
                 ->get()->first();
@@ -59,52 +59,52 @@ class NominaConceptController extends Controller
                 ->get()->first();
 
                 if(!empty($accounts)){
-                    $nominaconcept->account_code = $accounts->code_one.'.'.$accounts->code_two.'.'.$accounts->code_three.'.'.$accounts->code_four.'.'.str_pad($accounts->code_five, 3, "0", STR_PAD_LEFT); 
+                    $nominaconcept->account_code = $accounts->code_one.'.'.$accounts->code_two.'.'.$accounts->code_three.'.'.$accounts->code_four.'.'.str_pad($accounts->code_five, 3, "0", STR_PAD_LEFT);
                 } else {
-                    $nominaconcept->account_code = ''; 
+                    $nominaconcept->account_code = '';
                 }
 
                 if(!empty($accounts1)){
-                    $nominaconcept->account_code1 = $accounts1->code_one.'.'.$accounts1->code_two.'.'.$accounts1->code_three.'.'.$accounts1->code_four.'.'.str_pad($accounts1->code_five, 3, "0", STR_PAD_LEFT); 
+                    $nominaconcept->account_code1 = $accounts1->code_one.'.'.$accounts1->code_two.'.'.$accounts1->code_three.'.'.$accounts1->code_four.'.'.str_pad($accounts1->code_five, 3, "0", STR_PAD_LEFT);
                 } else {
-                    $nominaconcept->account_code1 = ''; 
+                    $nominaconcept->account_code1 = '';
                 }
                 if(!empty($accounts2)){
-                    $nominaconcept->account_code2 = $accounts2->code_one.'.'.$accounts2->code_two.'.'.$accounts2->code_three.'.'.$accounts2->code_four.'.'.str_pad($accounts2->code_five, 3, "0", STR_PAD_LEFT); 
+                    $nominaconcept->account_code2 = $accounts2->code_one.'.'.$accounts2->code_two.'.'.$accounts2->code_three.'.'.$accounts2->code_four.'.'.str_pad($accounts2->code_five, 3, "0", STR_PAD_LEFT);
                 } else {
-                    $nominaconcept->account_code2 = ''; 
+                    $nominaconcept->account_code2 = '';
                 }
                 if(!empty($accounts3)){
-                    $nominaconcept->account_code3 = $accounts3->code_one.'.'.$accounts3->code_two.'.'.$accounts3->code_three.'.'.$accounts3->code_four.'.'.str_pad($accounts3->code_five, 3, "0", STR_PAD_LEFT); 
+                    $nominaconcept->account_code3 = $accounts3->code_one.'.'.$accounts3->code_two.'.'.$accounts3->code_three.'.'.$accounts3->code_four.'.'.str_pad($accounts3->code_five, 3, "0", STR_PAD_LEFT);
                 } else {
-                    $nominaconcept->account_code3 = ''; 
+                    $nominaconcept->account_code3 = '';
                 }
                 if(!empty($accounts4)){
-                    $nominaconcept->account_code4 = $accounts4->code_one.'.'.$accounts4->code_two.'.'.$accounts4->code_three.'.'.$accounts4->code_four.'.'.str_pad($accounts4->code_five, 3, "0", STR_PAD_LEFT); 
+                    $nominaconcept->account_code4 = $accounts4->code_one.'.'.$accounts4->code_two.'.'.$accounts4->code_three.'.'.$accounts4->code_four.'.'.str_pad($accounts4->code_five, 3, "0", STR_PAD_LEFT);
                 } else {
-                    $nominaconcept->account_code4 = ''; 
+                    $nominaconcept->account_code4 = '';
                 }
                 if(!empty($accounts5)){
-                    $nominaconcept->account_code5 = $accounts5->code_one.'.'.$accounts5->code_two.'.'.$accounts5->code_three.'.'.$accounts5->code_four.'.'.str_pad($accounts5->code_five, 3, "0", STR_PAD_LEFT); 
+                    $nominaconcept->account_code5 = $accounts5->code_one.'.'.$accounts5->code_two.'.'.$accounts5->code_three.'.'.$accounts5->code_four.'.'.str_pad($accounts5->code_five, 3, "0", STR_PAD_LEFT);
                 } else {
-                    $nominaconcept->account_code5 = ''; 
+                    $nominaconcept->account_code5 = '';
                 }
 
 
-                
+
             }
         }
-    
+
         return view('admin.nominaconcepts.index',compact('nominaconcepts'));
-      
+
     }
 
     public function create()
     {
-       
+
         $date = Carbon::now();
         $datenow = $date->format('Y-m-d');
-        
+
         $formulam = NominaFormula::on(Auth::user()->database_name)
         ->where('type','M')
         ->orderBy('id','asc')->get();
@@ -124,7 +124,7 @@ class NominaConceptController extends Controller
         $formulaa = NominaFormula::on(Auth::user()->database_name)
         ->where('type','A')
         ->orderBy('id','asc')->get();
-    
+
         $accounts = '';
 
         $accounts_one = Account::on(Auth::user()->database_name)
@@ -163,9 +163,9 @@ class NominaConceptController extends Controller
 
     public function store(Request $request)
     {
-       
+
         $data = request()->validate([
-           
+
             'order'         =>'required',
             'abbreviation'  =>'required',
             'description'   =>'required|max:60',
@@ -173,13 +173,13 @@ class NominaConceptController extends Controller
             'sign'          =>'required',
 
             'calculate'     =>'required',
-           
+
 
             'minimum'     =>'required',
             'maximum'     =>'required',
-            
-            
-           
+
+
+
         ]);
 
         $users = new NominaConcept();
@@ -189,9 +189,9 @@ class NominaConceptController extends Controller
         $users->abbreviation = request('abbreviation');
         $users->description = request('description');
         $users->type = request('type');
-       
+
         $users->sign = request('sign');
-        
+
         $users->calculate = request('calculate');
         $users->id_formula_m = request('formula_m');
         $users->id_formula_s = request('formula_s');
@@ -211,8 +211,8 @@ class NominaConceptController extends Controller
 
 
         $users->status =  "1";
-       
-       
+
+
 
         $users->save();
 
@@ -286,22 +286,22 @@ class NominaConceptController extends Controller
 
        // dd($var);
         return view('admin.nominaconcepts.edit',compact('var','datenow','formulam','formulaq','formulas','formulae','formulaa','accounts','accounts_two','accounts_tree','accounts_one'));
-        
+
     }
 
-   
+
 
 
 
     public function update(Request $request,$id)
     {
-       
+
         $vars =  NominaConcept::on(Auth::user()->database_name)->find($id);
         $var_status = $vars->status;
-      
-      
+
+
         $data = request()->validate([
-           
+
             'order'         =>'required',
             'abbreviation'         =>'required',
             'description'   =>'required|max:60',
@@ -309,13 +309,13 @@ class NominaConceptController extends Controller
             'sign'          =>'required',
 
             'calculate'     =>'required',
-           
+
 
             'minimum'     =>'required',
             'maximum'     =>'required',
-            
-            
-           
+
+
+
         ]);
 
         $var = NominaConcept::on(Auth::user()->database_name)->findOrFail($id);
@@ -324,9 +324,9 @@ class NominaConceptController extends Controller
         $var->abbreviation = request('abbreviation');
         $var->description = request('description');
         $var->type = request('type');
-       
+
         $var->sign = request('sign');
-        
+
         $var->calculate = request('calculate');
         $var->id_formula_m = request('formula_m');
         $var->id_formula_s = request('formula_s');
@@ -335,12 +335,12 @@ class NominaConceptController extends Controller
         $var->id_formula_a = request('formula_a');
         $var->account_name = request('cuenta_contable');
 
-        $var->account_name1 = request('cuenta_contable1');
+       /* $var->account_name1 = request('cuenta_contable1');
         $var->account_name2 = request('cuenta_contable2');
         $var->account_name3 = request('cuenta_contable3');
         $var->account_name4 = request('cuenta_contable4');
-        $var->account_name5 = request('cuenta_contable5');
-        
+        $var->account_name5 = request('cuenta_contable5');*/
+
         $var->asignation = request('asignation');
         $var->prestations = request('prestations');
 
@@ -350,13 +350,13 @@ class NominaConceptController extends Controller
 
         $var->minimum = $valor_sin_formato_minimum;
         $var->maximum = $valor_sin_formato_maximum;
-       
+
         if(request('status') == null){
             $var->status = $var_status;
         }else{
             $var->status = request('status');
         }
-       
+
 
         $var->save();
 
